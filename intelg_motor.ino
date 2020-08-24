57d9f638f3ae6fb3b3c4a69dbdf501f903ae7beb
josephtjoshy <josephtjoshy9@gmail.com>
2020-08-24 16:30:41 +0530

    bugs fixing

--- intelg_motor.ino
+++ intelg_motor.ino
@@ -12,24 +12,20 @@ String line, dataProcess[50][11];
 bool devConnected = false;
 char tempReciveData[5000], recivedData[500][10];
 int i, j, temp = 0, dataTemp = 0, spaces = 0, k, Rtemp = 0;
-int hour[50], minutes[50], dmin[50], dsec[50], dmon[50], dtue[50], dwen[50], sthu[50], sfri[50], dsat[50], dsun[50], today;
-int rOn = 0, rOf = 0, timeInSec = 0, timeInHour = 0, timeInMin = 0;
-unsigned long old_time = 0;
+int hour[50], minutes[50], dmin[50], dsec[50], dmon[50], dtue[50], dwen[50], dthu[50], dfri[50], dsat[50], dsun[50], today;
+int rOn = 0, rOf = 0, timeInHour = 0, timeInMin = 0,timingNo,timingTemp[50],MotorOnMin=0,MotorOnSec=0;
+unsigned long old_time = 0,tempTime=0;
 unsigned long new_time, add_time, diff_time;
+float timeInSec = 0;
 
 void CalTime()
 {
 	if (timeInSec > 59)
 	{
-		new_time = 0;
-		add_time = 0;
+		
 		timeInMin++;
-	}
-	if (timeInMin > 59)
-	{
-		timeInMin = 0;
-		timeInHour++;
-	}
+		MotorOnMin--;
+	}	
 	if (timeInHour > 23)
 	{
 		timeInHour = 0;
@@ -38,6 +34,14 @@ void CalTime()
 	if (today > 7)
 	{
 		today = 1;
+	}
+	if(MotorOnMin<1)
+	{
+		MotorOnMin=0;
+	}
+	if(MotorOnSec<1)
+	{
+		MotorOnSec=0;
 	}
 }
 
@@ -74,6 +78,7 @@ void WiFiEvent(WiFiEvent_t event)
 
 	case SYSTEM_EVENT_AP_STADISCONNECTED:
 		//xEventGroupSetBits(event_group, STA_DISCONNECTED_BIT);
+		new_time = millis();
 		Serial.println("device disconnected");
 		devConnected = false;
 
@@ -117,13 +122,13 @@ void loop()
 				{
 					continue;
 				}
-				if (line[j] != '[' && line[j] != ']' && line[j] != '.' && line[j] != ',')
+				if (line[j] != '[' && line[j] != ']' && line[j] != '.' && line[j] != ',' && line[j]!='-' && line[j]!=':')
 				{
 					tempReciveData[temp] = line[j];
 					//Serial.print(tempReciveData[temp]);
 					temp++;
 				}
-				if (line[j] == '[' || line[j] == ']' || line[j] == '.' || line[j] == ',')
+				if (line[j] == '[' || line[j] == ']' || line[j] == '.' || line[j] == ',' || line[j]=='-' || line[j]==':')
 				{
 					tempReciveData[temp] = ' ';
 					//Serial.print(tempReciveData[temp]);
@@ -171,6 +176,32 @@ void loop()
 			{
 				//Serial.println("hourwise usage");
 				today = atoi(recivedData[k]);
+				timeInSec=atoi(recivedData[k-2]);
+				timeInMin=atoi(recivedData[k-3]);
+				timeInHour=atoi(recivedData[k-4]);
+				int rem1=timeInHour%10;
+				timeInHour=timeInHour/10;
+				int rem2=timeInHour%10;
+				timeInHour=(rem2*10)+rem1;
+				timingNo=(k-8)/11;
+				for(int f=0,g=2;f<timingNo;f++,g+=11)
+				{
+					hour[f]=atoi(recivedData[g]);
+					minutes[f]=atoi(recivedData[g+1]);
+					dmin[f]=atoi(recivedData[g+2]);
+					dsec[f]=atoi(recivedData[g+3]);
+					dmon[f]=atoi(recivedData[g+4]);
+					dtue[f]=atoi(recivedData[g+5]);
+					dwen[f]=atoi(recivedData[g+6]);
+					dthu[f]=atoi(recivedData[g+7]);
+					dfri[f]=atoi(recivedData[g+8]);
+					dsat[f]=atoi(recivedData[g+9]);
+					dsun[f]=atoi(recivedData[g+10]);
+					timingTemp[f]=1;
+				}
+
+
+				
 			}
 
 			//Serial.println(today);
@@ -187,7 +218,7 @@ void loop()
 			delay(100);
 			diff_time = diff_time + (new_time - old_time);
 			old_time = new_time;
-
+			new_time = millis();
 			if (diff_time > 1000)
 			{
 				Rtemp = Rtemp + (diff_time / 1000);
@@ -210,12 +241,56 @@ void loop()
 		}
 		else if (recivedData[1][0] == 'l')
 		{
+			
+			delay(100);
 			add_time = add_time + (new_time - old_time);
+			tempTime=tempTime+(new_time-old_time);
+			if((tempTime/1000)>=1)
+			{
+				MotorOnSec--;
+				tempTime=0;
+			}
+			old_time = new_time;
+			new_time = millis();
+			Serial.print("add");
+			Serial.println(add_time);
 			timeInSec = add_time / 1000;
+			timeInMin=(add_time/1000)/60;
+			timeInHour=timeInHour+((add_time/1000)/3600);
+			if(timeInHour%2==0)
+			{
+				add_time=0;
+			}			
 			CalTime();
+			Serial.print(timeInHour);
+			Serial.print(":");
+			Serial.print(timeInMin);
+			Serial.print(":");
+			Serial.println(timeInSec);
+			for(int f=0;f<timingNo;f++)
+			{
+				if(timeInHour>=hour[f] && timingTemp[f]==1)
+				{
+					if(timeInMin>minutes[f])
+					{
+						MotorOnMin=dmin[f];
+						MotorOnSec=dsec[f];
+						timingTemp[f]=0;
+					}
+				}
+			}
+			if(MotorOnSec>0 || MotorOnMin>0)
+			{
+				Serial.println("Motor On");
+			}
+			else
+			{
+				Serial.println("Motor off");
+			}
+			
 		}
 	}
 
-	new_time = millis();
+	
 	
 }
